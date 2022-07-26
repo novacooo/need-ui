dfc62f93b120ef7ecf1d1eabd94c82481bf14893
Jacek Nowak <jaceknowak226@gmail.com>
2022-07-26 22:27:14 +0200

    Add styled-components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import './Button.css';
 
 export interface ButtonProps {
   label: string;
 }
 
+const StyledButton = styled.button`
+  background-color: red;
+
+  &:hover {
+    cursor: pointer;
+    background-color: blue;
+  }
+`;
+
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <StyledButton>{props.label}</StyledButton>;
 };
 
 export default Button;
