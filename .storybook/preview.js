372045f7ec6c81212cdc47af4e65a563f385d0bf
Jacek Nowak <jaceknowak226@gmail.com>
2022-07-28 21:40:21 +0200

    Add NeedProvider to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { NeedProvider } from '../src';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +10,11 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <NeedProvider>
+      <Story />
+    </NeedProvider>
+  ),
+];
