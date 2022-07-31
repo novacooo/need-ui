d863ad81a0f44f60f095cdca65ab5c4034dd6949
Jacek Nowak <jaceknowak226@gmail.com>
2022-07-31 23:30:27 +0200

    Theme breakpoints

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,4 +1,11 @@
 export interface ITheme {
+  breakpoints: {
+    sm: string;
+    md: string;
+    lg: string;
+    xl: string;
+    '2xl': string;
+  };
   background: {
     primary: string;
   };
@@ -11,6 +18,13 @@ export interface ITheme {
 }
 
 export const defaultDarkTheme: ITheme = {
+  breakpoints: {
+    sm: '@media (min-width: 640px)',
+    md: '@media (min-width: 768px)',
+    lg: '@media (min-width: 1024px)',
+    xl: '@media (min-width: 1280px)',
+    '2xl': '@media (min-width: 1536px)',
+  },
   background: {
     primary: 'hsl(241, 4%, 9%)',
   },
